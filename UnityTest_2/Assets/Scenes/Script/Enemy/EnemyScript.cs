04c8c509381cd17b7d43c089915a1476e477006f
StefanoManescotto <stefa.manescotto@gmail.com>
2019-12-24 11:02:59 +0100

    create nuove stanze e aggiunte collisioni, inizio script generatore stanze

---
+++ UnityTest_2/Assets/Scenes/Script/Enemy/EnemyScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyScript : MonoBehaviour
+{
+
+    private int vita = 100;
+
+    void Update()
+    {
+        //TODO: Inseguimento Giocatore
+    }
+
+    void OnTriggerEnter2D(Collider2D Other)
+    {
+        if (Other.gameObject.tag == "Proiettili")
+        {
+            Destroy(Other.gameObject);
+            vita -= 50;
+            if(vita <= 0)
+            {
+                Destroy(gameObject);
+            }
+        }
+    }
+
+}
---
+++ UnityTest_2/Assets/Scenes/Script/Room/RoomGenScript.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoomGenScript : MonoBehaviour
+{
+
+    [SerializeField]
+    private GameObject[] stanze;
+
+    [SerializeField]
+    private int numeroStanze = 5;
+
+    private List<GameObject> stanzeCreate;
+
+    void Start()
+    {
+        for(int i=0; i<numeroStanze; i++)
+        {
+            if(i == 0)
+            {
+                stanzeCreate.Add(Instantiate(stanze[0], Vector3.zero, Quaternion.identity));
+            }else if (i < numeroStanze)
+            {
+                //stanze tb
+            }
+            else
+            {
+                //stanze b
+            }
+        }
+    }
+
+    void Update()
+    {
+        
+    }
+
+    void posizioneStanzaSuccessiva()
+    {
+        //TODO: aggiungere Gameobject spawner alle stanze
+        //TODO: stanzaCreate --> trova ultima stanza aggiunta, fare il getComponent allo spawner delle stanze e spawnare prossima stanza in quella posizione
+    }
+
+}
